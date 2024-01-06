4577c75c68ef81f3055865fa34fdd7a2a51fa8e3
gravegrow <gravegrow@gmail.com>
2024-01-06 19:36:38 +0300

    subprocces pritty traceback

---
+++ screencap/__init__.py
@@ -0,0 +1,3 @@
+from screencap.capture import WindowCapture
+from screencap.geometry import Geometry
+from screencap.image import Image
--- screencap/window.py
+++ screencap/window.py
@@ -2,12 +2,11 @@ import subprocess
 from dataclasses import dataclass, field
 from typing import List
 
-from rich import box
-from rich.console import Console
-from rich.prompt import IntPrompt
-from rich.table import Table
+from rich import box, console, prompt, table, traceback
 
 from screencap.geometry import WindowGeometry
+
+traceback.install(max_frames=1)
 
 
 @dataclass
@@ -20,10 +19,16 @@ class Window:
         return self._get_geometry(self._pid)
 
     def choose(self):
-        self._pid = self._pick_pid()
+        pids = self._find_pids()
+        self._pid = self._choose_pid(pids)
 
     def _get_geometry(self, pid: str):
-        out = subprocess.check_output(["xdotool", "getwindowgeometry", str(pid)], text=True)
+        try:
+            out = subprocess.check_output(["xdotool", "getwindowgeometry", pid], text=True)
+        except subprocess.CalledProcessError as e:
+            console.Console().log(f"Process '{self.process} was terminated.")
+            raise e.with_traceback(None)
+
         out = out.split()
 
         x, y = out[3].split(",")
@@ -31,32 +36,34 @@ class Window:
 
         return WindowGeometry(int(x), int(y), int(width), int(height))
 
-    def _pick_pid(self) -> str:
-        pids = self._find_pids()
-
-        table = Table(box=box.ROUNDED, header_style="green dim")
-        table.add_column("Index", justify="center", style="blue dim", width=len("Index"))
-        table.add_column("PID", justify="center", style="dim", width=12)
+    def _choose_pid(self, pids: List[str]) -> str:
+        tbl = table.Table(box=box.ROUNDED, header_style="green dim")
+        tbl.add_column("Index", justify="center", style="blue dim", width=len("Index"))
+        tbl.add_column("PID", justify="center", style="dim", width=12)
 
         for index, pid in enumerate(pids):
-            table.add_row(str(index + 1), str(pid))
+            tbl.add_row(str(index + 1), str(pid))
 
-        Console().print(table)
-        choice = IntPrompt.ask("Pick", choices=[str(i + 1) for i in range(len(pids))])
+        console.Console().print(tbl)
+        choice = prompt.IntPrompt.ask("Pick", choices=[str(i + 1) for i in range(len(pids))])
 
         return pids[int(choice) - 1]
 
     def _find_pids(self) -> List[str]:
-        found = subprocess.check_output(
-            [
-                "xdotool",
-                "search",
-                "--onlyvisible",
-                "--classname",
-                self.process,
-            ],
-            text=True,
-        )
+        try:
+            found = subprocess.check_output(
+                [
+                    "xdotool",
+                    "search",
+                    "--onlyvisible",
+                    "--classname",
+                    self.process,
+                ],
+                text=True,
+            )
+        except subprocess.CalledProcessError as e:
+            console.Console().log(f"Process '{self.process}' not found.")
+            raise e
 
         pids = []
         for pid in found.split("\n"):
