ee918c77ee96a7dd1fbe257fdca4463a37cbae74
gravegrow <gravegrow@gmail.com>
2024-01-26 16:21:13 +0300

    viewer exposed

--- screencap/__init__.py
+++ screencap/__init__.py
@@ -2,3 +2,4 @@ from screencap.capture import WindowCapture
 from screencap.image import image
 from screencap.image.area import Area
 from screencap.previews import Previews, SelectPidPreviews
+from screencap.viewer import Viewer
